debcac43957340b04e23db856e737aeb4965b28d
Daniel Lara <larad@uci.edu>
2017-04-05 16:22:52 -0700

    04/05/2017 4:22PM
    * Created a singleton class for all manager type classes to inherit from.
    * Created an input manager which processes all available player inputs in one place per frame.
    * Created a controllable object class. This class is used by the input manager to look for specific inputs per player. As of right now, we only have one player, but the implementation can accommodate as many as we like.

---
+++ ICS168/Assets/Scripts/Input Manager/InputManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ButtonEnum {
+    MoveRight,
+    MoveLeft,
+    MoveUp,
+    MoveDown,
+    DeployBomb
+}
+
+public enum ConditionEnum {
+    GreaterThanOffValue,
+    LessThanOffValue
+}
+
+[System.Serializable]
+public class InputAxisState {
+
+    [SerializeField]
+    private string _axisName;           // Which axis will this input be looking for.
+    [SerializeField]
+    private ButtonEnum _button;         // Which button will be mapped to the assigned axis
+    public ButtonEnum Button {
+        get { return _button; }
+    }
+
+    [SerializeField]
+    private float _offValue;            // when should it stop reading input from the player.
+    [SerializeField]
+    private ConditionEnum _condition;   // When should it starting reading input from the player.
+
+    public bool IsPressed {
+        get {
+            float isPressed = 0.0f;
+
+            isPressed = Input.GetAxis(_axisName);
+
+            switch (_condition) {
+                case ConditionEnum.GreaterThanOffValue:
+                    return isPressed > _offValue;
+                case ConditionEnum.LessThanOffValue:
+                    return isPressed < _offValue;
+            }
+
+            return false;
+        }
+    }
+}
+
+public class InputManager : Singleton<InputManager> {
+
+    [SerializeField]
+    private ControllableObject _player;
+    [SerializeField]
+    private InputAxisState[] _inputs;
+
+    public void Update() {
+
+        foreach(InputAxisState input in _inputs) {
+            _player.SetButtonStates(input.Button, input.IsPressed);
+        }
+    }
+}
---
+++ ICS168/Assets/Scripts/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<Type> : MonoBehaviour where Type : MonoBehaviour {
+
+    private static GameObject _instance;
+
+    public static GameObject Instance {
+        get {
+            // If the instance is null, look to see if there is already an instance in the scene
+            if (_instance == null) {
+                _instance = GameObject.FindGameObjectWithTag(typeof(Type).Name);
+
+                // If an instance could not be found, create one.
+                if (_instance == null) {
+                    _instance = Instantiate(Resources.Load("Managers/" + typeof(Type).Name, typeof(GameObject)) as GameObject, Vector3.zero, Quaternion.identity) as GameObject;
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake() {
+
+        if (_instance == null) {
+            _instance = GameObject.FindGameObjectWithTag(typeof(Type).Name);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
