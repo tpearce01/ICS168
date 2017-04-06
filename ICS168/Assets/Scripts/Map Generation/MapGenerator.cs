bf8577a9acf9c0a72cd3f8f25f037fb932457f03
Tyler Pearce <pearcetyler@yahoo.com>
2017-04-06 01:12:55 -0700

    Added Map Generator
    - Added MapGenerator script
    - Added Tile class
    - Created MapGenerator prefab
    - Created prefabs for Basic and Wall tiles
    - Moved some file locations to clean up assets folder
    - Added test map to resources folder

---
+++ ICS168/Assets/Scripts/Map Generation/MapGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public List<GameObject> tileTypes;
+
+	void Start () {
+		GenerateMap("TestMap");
+        Destroy(gameObject);
+	}
+
+    //Creates tiles and sets them to the appropriate locations
+    void GenerateMap(string fileName)
+    {
+        TextAsset txt = Resources.Load(fileName) as TextAsset;
+        //Debug.Log(txt.text);
+
+        string[] data = txt.text.Split('\n');
+        Tile temp;
+        for (int y = 0; y < data.Length; y++)
+        {
+            for (int x = 0; x < data[y].Length - 1; x++)
+            {
+                //Debug.Log(data[y][x]);
+                temp = (Instantiate(tileTypes[Int32.Parse(data[y][x].ToString())]) as GameObject).GetComponent<Tile>();
+                temp.x = x;
+                temp.y = y;
+                temp.SetLocation();
+            }
+        }
+
+        //Move camera to middle position
+        GameObject.Find("Main Camera").transform.position = new Vector3((data[0].Length - 1) / 2f,(data.Length-1) / 2f,-10);
+        //Change size based on tile dimensions
+    }
+
+
+}
---
+++ ICS168/Assets/Scripts/Map Generation/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public int x;
+    public int y;
+    public TileType type;
+
+    public void SetLocation()
+    {
+        gameObject.transform.position = new Vector2(x, y);
+    }
+}
+
+public enum TileType
+{
+    Wall = 0,
+    StartLocation = 1,
+    Basic = 2,
+}
