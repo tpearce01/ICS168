03e53abb1201fc82b2cab0b618b9269ddc4b595f
Zachary Hart <zachary.hart825@yahoo.com>
2017-04-05 21:45:11 -0700

    04/05/2017 9:42PM - Tweaks to game manager and added some prefabs
    Added a player prefab inside that are:
    - A player script, which...
        - changes the color of the player depending on its assigned number
        - tracks if the player is alive or not
    
    Tweaked the game manager to
    - Add public getters
    - Track a winner (may need fine-tuning) once time runs out
    - Still need to add once players are all dead

--- ICS168/Assets/Scripts/GameManager.cs
+++ ICS168/Assets/Scripts/GameManager.cs
@@ -25,7 +25,16 @@ public class GameManager : MonoBehaviour {
     //This attribute is used to track if the game is over or not. Should not appear in the inspector.
     private bool _isGameOver;
 
+    //Player attributes.
+    /* These attributes are used to track the game objects that will represent the players.
+     * I'm guessing that we will be having 4 players in the networked version, similar to the normal bomberman game.
+     *
+     * I'll have 4 slots in there for now, but we can leave the other three empty OR have them be dummy characters the player needs to kill.
+    */
+    [SerializeField]
+    private GameObject[] _players;
 
+    private int winner = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -36,11 +45,24 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        //Count down the timer
+        _currTime -= Time.deltaTime;
+
+        if (_currTime < 0.0f)
+        {
+            _isGameOver = true;
+        }
+
+        if (_isGameOver)
+        {
+            //Do whatever you need to do once the game is over. 
+            winner = findWinner();
+        }
 	}
 
     //Call this function to decrement the number of alive players by 1.
-    void decAlivePlayers()
+    public void decAlivePlayers()
     {
         _numOfAlivePlayers -= 1;
     }
@@ -50,33 +72,54 @@ public class GameManager : MonoBehaviour {
     */
 
     //Returns the number of players in the game.
-    int getNumOfPlayers()
+    public int getNumOfPlayers()
     {
         return _numOfPlayers;
     }
 
     //Returns the number of players that are still alive in this game.
-    int getNumOfAlivePlayers()
+    public int getNumOfAlivePlayers()
     {
         return _numOfAlivePlayers;
     }
 
     //Returns the round time set.
-    float getRoundTime()
+    public float getRoundTime()
     {
         return _roundTime;
     }
 
     //Returns the current time in the match.
-    float getCurrentTime()
+    public float getCurrentTime()
     {
         return _currTime;
     }
 
     //Returns a bool stating whether or not the game is over.
-    bool getIsGameOver()
+    public bool getIsGameOver()
     {
         return _isGameOver;
+    }
+
+    //Returns the player number of the winner, 0 if there is a draw.
+    int findWinner()
+    {
+        int numOfWinners = 0;
+        int winnerNumber = 0;
+        for (int i = 0; i < _players.Length; i++)
+        {
+            if (_players[i].GetComponent<PlayerScript>().isAlive())
+            {
+                ++numOfWinners;
+                winnerNumber = _players[i].GetComponent<PlayerScript>().getPlayerNumber();
+            }
+            if (numOfWinners > 1)
+            {
+                winnerNumber = 0;
+                break;
+            }
+        }
+        return winnerNumber;
     }
 }
 
---
+++ ICS168/Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    // Based on the player's number, it will color the sprite differently!
+    // Player's number should be between 1 and 4.
+    [Range(1,4)]
+    public int playerNumber;
+
+    private SpriteRenderer _sr;
+
+    //This attribute tracks whether or not the player is alive or not.
+    private bool _isAlive;
+
+	// Use this for initialization
+	void Start () {
+        _isAlive = true;
+        _sr = gameObject.GetComponent<SpriteRenderer>();
+        setPlayerColor(_sr);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!_isAlive)
+        {
+            //Don't do anything!
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Explosion")
+        {
+            _isAlive = false;
+        }
+    }
+    //Sets the color of the player based on the player number.
+    void setPlayerColor(SpriteRenderer sr)
+    {
+        switch (playerNumber)
+        {
+            case 1:
+                sr.material.SetColor("_Color", Color.red);
+                break;
+            case 2:
+                sr.material.SetColor("_Color", Color.blue);
+                break;
+            case 3:
+                sr.material.SetColor("_Color", Color.green);
+                break;
+            case 4:
+                sr.material.SetColor("_Color", Color.yellow);
+                break;
+        }
+    }
+
+    //Returns if the player is alive.
+    public bool isAlive()
+    {
+        return _isAlive;
+    }
+
+    public int getPlayerNumber()
+    {
+        return playerNumber;
+    }
+}
