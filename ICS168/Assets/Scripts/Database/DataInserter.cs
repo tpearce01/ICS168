e75f20d4b94f8f570b90d42610f15346ba217dd5
Ney Congjuico <ncongjuico@yahoo.com>
2017-04-19 19:11:43 -0700

    04/19/17 7:12PM
    * Created a DataLoader script to fetch data from the database (login info)
    * Created a DataInserter script to insert things into the database (new accounts)
    *Note: You need to setup the local database before either of the scripts can work. I'll post the instructions on discord.

---
+++ ICS168/Assets/Scripts/Database/DataInserter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataInserter : MonoBehaviour {
+    public string inputUsername;
+    public string inputPassword;
+
+    private string CreateAccountURL = "http://localhost/teamnewport/CreateAccount.php";
+    
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) CreateUser(inputUsername, inputPassword);
+	}
+
+    public void CreateUser(string username, string password){
+        //WWWForm allows to communicate with php files
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("passwordPost", password);
+
+        WWW www = new WWW(CreateAccountURL, form);
+    }
+}
---
+++ ICS168/Assets/Scripts/Database/DataLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour {
+    public string[] users;
+
+    IEnumerator Start() {
+        WWW userData = new WWW("http://localhost/teamnewport/UserData.php");
+        yield return userData; //wait for userData to finish downloading
+        string userDataString = userData.text; //store text on website in string
+
+        //Debug.Log(userDataString);
+        users = userDataString.Split(';');
+        Debug.Log(GetDataValue(users[0], "username:"));
+    }
+
+    string GetDataValue(string data, string index){
+        string value = data.Substring(data.IndexOf(index)+index.Length); //get everything after index
+        if (value.Contains("|")){
+            value = value.Remove(value.IndexOf("|")); // remove everything after |
+        }
+        return value;
+    }
+}
