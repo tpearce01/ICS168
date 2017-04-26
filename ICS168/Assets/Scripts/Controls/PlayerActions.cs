3d80e72e45dfb1d1bf2150ad4e7904c52a2dc3e3
Daniel Lara <LaraD@uci.edu>
2017-04-26 15:32:58 -0700

    04/26/2017 3:32PM
    * Fixed a merge conflict with PlayerActions script.

--- ICS168/Assets/Scripts/Controls/PlayerActions.cs
+++ ICS168/Assets/Scripts/Controls/PlayerActions.cs
@@ -1,60 +1,16 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 
 /// <summary>
 /// This script is for all player actions which includes walking horizontal, wakling vertical, and deploying bombs.
 /// </summary>
 public class PlayerActions : MonoBehaviour {
 
-    private ControllableObject _inputHandler;
     private int _playerNum = 1;
-
     private Vector3 _pos;
-
-    private void OnEnable() {
-        if (_inputHandler == null) {
-            _inputHandler = GetComponent<ControllableObject>();
-        }
-    }
-
-    private void Start() {
-
-        InputManager.Instance.AddPlayer(GetComponent<ControllableObject>(),
-            Resources.Load("Input/P" + _playerNum + "InputList", typeof(SOInputList)) as SOInputList);
-    }
-
-    private void Update() {
-        if (_inputHandler.OnButtonDown(ButtonEnum.DeployBomb)) {
-            ClientConnection.Instance.DeployBomb();
-        }
-
-        _pos = gameObject.transform.position;
-
-        //RIGHT
-        if (_inputHandler.OnButtonDown(ButtonEnum.MoveRight)) {
-            _pos.x += 1;
-        }
-
-        //LEFT
-        if (_inputHandler.OnButtonDown(ButtonEnum.MoveLeft)) {
-            _pos.x -= 1;
-        }
-
-        //UP
-        if (_inputHandler.OnButtonDown(ButtonEnum.MoveUp)) {
-            _pos.y += 1;
-        }
-
-        //DOWN
-        if (_inputHandler.OnButtonDown(ButtonEnum.MoveDown)) {
-            _pos.y -= 1;
-        }
-
-        if (ValidPos(_pos)) {
-            gameObject.transform.position = _pos;
-        }
-    }
 
     bool ValidPos(Vector3 pos) {
 
@@ -66,22 +22,17 @@ public class PlayerActions : MonoBehaviour {
                 return false;
             }
         }
-
-        return true;
+            return true;
     }
+        //Al's test code
 
-   
-    //Al's test code
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("PowerUp"))
-        {
+        if (other.CompareTag("PowerUp")) {
             Destroy(other.gameObject);
             ReplaceWithBasicTile();
-            
-            
-
         }
+
     }
 
     void ReplaceWithBasicTile()
@@ -93,108 +44,26 @@ public class PlayerActions : MonoBehaviour {
         MapGenerator.Instance.tileMap[temp.x, temp.y] = temp;
     }
 
-    private void DeployBomb() {
-        Spawner.Instance.GetComponent<Spawner>().SpawnObject(Prefab.Bomb, new Vector3(Mathf.Round(gameObject.transform.position.x), Mathf.Round(gameObject.transform.position.y), 0));
-    }
-
-    private void Walking() {
-
+    public void RequestAction(PlayerIO command) {
         _pos = gameObject.transform.position;
-
-        //RIGHT
-        if (_inputHandler.OnButtonDown(ButtonEnum.MoveRight)) {
-            _pos.x += 1;
+        switch (command.button) {
+            case ButtonEnum.DeployBomb:
+                Spawner.Instance.GetComponent<Spawner>().SpawnObject(Prefab.Bomb, new Vector3(Mathf.Round(gameObject.transform.position.x), Mathf.Round(gameObject.transform.position.y), 0));
+                break;
+            case ButtonEnum.MoveDown:
+                _pos.y -= 1;
+                break;
+            case ButtonEnum.MoveUp:
+                _pos.y += 1;
+                break;
+            case ButtonEnum.MoveLeft:
+                _pos.x -= 1;
+                break;
+            case ButtonEnum.MoveRight:
+                _pos.x += 1;
+                break;
         }
 
-        //LEFT
-        if (_inputHandler.OnButtonDown(ButtonEnum.MoveLeft)) {
-            _pos.x -= 1;
-        }
-
-        //UP
-        if (_inputHandler.OnButtonDown(ButtonEnum.MoveUp)) {
-            _pos.y += 1;
-        }
-
-        //DOWN
-        if (_inputHandler.OnButtonDown(ButtonEnum.MoveDown)) {
-            _pos.y -= 1;
-        }
-
-        if (ValidPos(_pos)) {
-            gameObject.transform.position = _pos;
-        }
+        if (ValidPos(_pos)) { gameObject.transform.position = _pos; }
     }
 }
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-/// <summary>
-/// This script is for all player actions which includes walking horizontal, wakling vertical, and deploying bombs.
-/// </summary>
-public class PlayerActions : MonoBehaviour {
-
-    private int _playerNum = 1;
-
-    private Vector3 _pos;
-
-    bool ValidPos(Vector3 pos) {
-
-        Tile[,] tileMap = MapGenerator.Instance.tileMap;
-
-        if (tileMap != null) {
-            Tile tile = tileMap[(int)pos.x, (int)pos.y].GetComponent<Tile>();
-            if (tile.type == TileType.Wall || tile.type == TileType.Destructable || tile.type == TileType.WallPowerUp) {
-                return false;
-            }
-        return true;
-    }
-
-   
-    //Al's test code
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("PowerUp"))
-        {
-            Destroy(other.gameObject);
-            ReplaceWithBasicTile();
-            
-            
-
-        }
-    }
-
-    void ReplaceWithBasicTile()
-    {
-        Tile temp = Instantiate(MapGenerator.Instance.tileTypes[(int)TileType.Basic]).GetComponent<Tile>();
-        temp.x = (int)gameObject.transform.position.x;
-        temp.y = (int)gameObject.transform.position.y;
-        temp.SetLocation();
-        MapGenerator.Instance.tileMap[temp.x, temp.y] = temp;
-    }
-
-    public void RequestAction(PlayerIO command) {
-
-        _pos = gameObject.transform.position;
-
-        switch (command.button) {
-            case ButtonEnum.DeployBomb:
-                Spawner.Instance.GetComponent<Spawner>().SpawnObject(Prefab.Bomb, new Vector3(Mathf.Round(gameObject.transform.position.x), Mathf.Round(gameObject.transform.position.y), 0));
-                break;
-            case ButtonEnum.MoveDown:
-                _pos.y -= 1;
-                break;
-            case ButtonEnum.MoveUp:
-                _pos.y += 1;
-                break;
-            case ButtonEnum.MoveLeft:
-                _pos.x -= 1;
-                break;
-            case ButtonEnum.MoveRight:
-                _pos.x += 1;
-                break;
-        }
-
-        if (ValidPos(_pos)) { gameObject.transform.position = _pos; }
-    }
