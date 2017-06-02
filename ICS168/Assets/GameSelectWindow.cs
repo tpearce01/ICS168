9f621e4525ab1e673518370c6301ebd4e866182d
Ney Congjuico <ncongjuico@yahoo.com>
2017-06-02 00:41:58 -0700

    Disabled join button

--- ICS168/Assets/GameSelectWindow.cs
+++ ICS168/Assets/GameSelectWindow.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameSelectWindow : GenericWindow {
+    [SerializeField]
+    Button JoinButton;
 
     public void OnBackToMain() {
         WindowManager.Instance.ToggleWindows(WindowIDs.GameSelect, WindowIDs.StartWindow);
     }
 
+    //If network drops the message, client is fucked and can never play the game ever again
     public void OnJoinGame() {
         Text[] input = gameObject.GetComponentsInChildren<Text>();
         string serverName = input[3].text;
 
         Debug.Log("Trying to connect to: " + serverName);
         ClientConnection.Instance.ConnectToGameServer(serverName);
+        
+        JoinButton.interactable = false;
     }
 }
